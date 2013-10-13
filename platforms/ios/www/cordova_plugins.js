c1a8fba4f7569596c0dfb01fbb83371c1f4bb60c
codywood010 <codywood010@gmail.com>
2013-10-12 19:24:55 -0600

    Remove inappbrowser and snapshot

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,11 +1,3 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/org.apache.cordova.inappbrowser/www/InAppBrowser.js",
-        "id": "org.apache.cordova.inappbrowser.InAppBrowser",
-        "clobbers": [
-            "window.open"
-        ]
-    }
-]
+module.exports = []
 });
